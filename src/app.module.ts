7f442dcc30d9614cb9452feb3afc6065ebcc5d47
Argie <argie.b@flowerstore.ph>
2024-04-15 14:46:34 +0800

    add user authentication feature

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { ConfigModule } from '@nestjs/config';
+import { RootModule } from './root/root.module';
+import { AuthModule } from './auth/auth.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true
+    }), 
+    RootModule, 
+    AuthModule, 
+    PrismaModule, 
+    UserModule
+  ],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  app.useGlobalPipes(new ValidationPipe({ stopAtFirstError: true, whitelist: true }))
+  
+  if(process.env.API === 'DEV') {
+    await app.listen(3001);
+  }
+
+  if(process.env.API === 'PROD') {
+    await app.listen(process.env.PORT);
+  }
+
 }
 bootstrap();
